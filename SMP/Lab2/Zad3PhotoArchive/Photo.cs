46345ba7bd1f0d986f167f5d0e7eadc96108a450
IvoRum <ivailo.rumenov@outlook.com>
2024-03-04 19:24:18 +0200

    цреатед неш

---
+++ SMP/Lab2/Zad3PhotoArchive/Photo.cs
@@ -0,0 +1,32 @@
+﻿namespace Zad3PhotoArchive;
+
+public class Photo
+{
+    private DateTime takenOn;
+    private String data;
+
+    public DateTime TakenOn
+    {
+        get => takenOn;
+        set => takenOn = value;
+    }
+
+    public string Data
+    {
+        get => data;
+        set => data = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public Photo ShallowCopy()
+    {
+        return (Photo)this.MemberwiseClone();
+    }
+
+    public Photo DeepClone()
+    {
+        Photo clone = (Photo)this.MemberwiseClone();
+        clone.TakenOn = new DateTime(takenOn.Ticks);
+        clone.data = String.Copy(data);
+        return clone;
+    }
+}
