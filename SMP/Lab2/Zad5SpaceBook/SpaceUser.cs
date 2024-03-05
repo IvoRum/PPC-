e44067e62d70a440b57d3f881736aa2bf66955d0
irumenov2 <ivaylo.rumenov2@dxc.com>
2024-03-05 10:07:08 +0200

    Finished zad 5 from lab 2

---
+++ SMP/Lab2/Zad5SpaceBook/SpaceUser.cs
@@ -0,0 +1,14 @@
+﻿namespace Zad5SpaceBook;
+
+public class SpaceUser
+{
+    private string name;
+
+    public SpaceGroup createGroup(List<SpaceUser> admins, List<SpaceUser> users, List<SpaceGroup> moreGroupsLikeDis)
+    {
+        Console.WriteLine("What is the title of the group?");
+        string title = Console.In.ReadLine();
+        SpaceGroup nSpaceGroup = new SpaceGroup(title,admins,users,moreGroupsLikeDis);
+        return nSpaceGroup;
+    }
+}
