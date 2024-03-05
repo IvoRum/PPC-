b14a3dcc5040c883b050bd7f15dace1cbc42b477
IvoRum <ivailo.rumenov@outlook.com>
2024-03-06 00:16:45 +0200

    Lab 8 is done

---
+++ SMP/lab8/Zad2/Supervisor.cs
@@ -0,0 +1,28 @@
+﻿namespace Zad2;
+
+public class Supervisor : IEmployee
+{
+    private string name;
+    private int happiness;
+    private List<IEmployee> subordinate = new List<IEmployee>();
+
+    public Supervisor(string name, int happiness)
+    {
+        this.name = name;
+        this.happiness = happiness;
+    }
+
+    public void ShowHappiness()
+    {
+        Console.WriteLine(name + " showed happiness level of " + happiness);
+        foreach (IEmployee i in subordinate)
+        {
+            i.ShowHappiness();
+        }
+    }
+
+    public void AddSubordinate(IEmployee employee)
+    {
+        subordinate.Add(employee);
+    }
+}
---
+++ SMP/lab8/Zad2/Worker.cs
@@ -0,0 +1,23 @@
+﻿namespace Zad2;
+
+public class Worker:IEmployee
+{
+    private string name;
+    private int happiness;
+
+    public Worker(string name, int happiness)
+    {
+        this.name = name;
+        this.happiness = happiness;
+    }
+
+    public void ShowHappiness()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IEmployee.ShowHappiness()
+    {
+        Console.WriteLine(name + " showed happiness level of " + happiness); 
+    }
+}
