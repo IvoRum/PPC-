be6ceda6f57bc237e3ecc332337296cd44b7e149
irumenov2 <ivaylo.rumenov2@dxc.com>
2024-03-05 09:04:16 +0200

    Finished singolton

---
+++ SMP/Lab1/Lab1/Zad3Balansator/Balancer.cs
@@ -0,0 +1,34 @@
+﻿namespace Zad3Balansator;
+
+public class Balancer
+{
+    private static Balancer instance = null;
+    private List<Server> Servers;
+    
+    private Balancer()
+    {
+    }
+    
+    public static Balancer GetInstance()
+    { 
+        if (instance == null) 
+        {
+                instance = new Balancer();
+        } 
+        return instance;
+    }
+
+    void sendMessage(string ms)
+    {
+        int lowestWorkFlow=0;
+        Server serverWhitHiestWorkFlow=new Server();
+        foreach (var server in Servers)
+        {
+            if (lowestWorkFlow <server.WorkFlow1)
+            {
+                serverWhitHiestWorkFlow = server;
+            }
+        }
+        serverWhitHiestWorkFlow.work(ms);
+    }
+}
