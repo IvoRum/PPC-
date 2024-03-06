d8e66529c58c0f86ed1ef49b4eb559fc2d5011a3
IvoRum <ivailo.rumenov@outlook.com>
2024-03-06 21:55:57 +0200

    Nine build pattern is done

---
+++ SMP/Lab9/Zad2/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.ComponentModel;
+using Zad2;
+
+public class Manager
+{
+    static void Main()
+    {
+        Icomponent a = new PlainIceCream(); 
+        Icomponent b = new CandyTopping(a); 
+        Icomponent c = new PeanutTopping(b); 
+        Icomponent d = new NutsTopping(c); 
+        
+        d.AddTopping();
+    }
+}
---
+++ SMP/Lab9/Zad2/Topping.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+
+namespace Zad2;
+
+public class Topping:Icomponent
+{
+    internal Icomponent input;
+
+    public Topping(Icomponent input)
+    {
+        this.input = input;
+    }
+
+    public void AddTopping()
+    {
+        input.AddTopping();
+    }
+}
