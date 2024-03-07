ddf914a245e51679378a429ba2f84e48bdb69c62
IvoRum <ivailo.rumenov@outlook.com>
2024-03-07 21:02:50 +0200

    finished facade

---
+++ SMP/Lab10Facade/Zad1/Programmer.cs
@@ -0,0 +1,10 @@
+﻿namespace Zad1;
+
+public class Programmer
+{
+    public void CreateApplication(VisualStudioFacade facade)
+    {
+        facade.Start(); 
+        facade.Stop();
+    }
+}
---
+++ SMP/Lab10Facade/Zad1/TextEditor.cs
@@ -0,0 +1,14 @@
+﻿namespace Zad1;
+
+public class TextEditor
+{
+    public void CreateCode()
+    {
+        Console.WriteLine("Napishi kod");
+    }
+
+    public void Save()
+    {
+        Console.WriteLine("Saved Code");
+    }
+}
