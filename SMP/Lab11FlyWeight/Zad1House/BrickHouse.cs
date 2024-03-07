c9c7385c82c3b5876ec4631b9094fd888d7cb6de
IvoRum <ivailo.rumenov@outlook.com>
2024-03-07 22:40:24 +0200

    Flyweth done and dusted

---
+++ SMP/Lab11FlyWeight/Zad1House/BrickHouse.cs
@@ -0,0 +1,14 @@
+﻿namespace Zad1House;
+
+public class BrickHouse:House
+{
+    public BrickHouse()
+    {
+        stages = 5;
+    }
+
+    public override void Build(double longitude, double latitude)
+    {
+        Console.WriteLine("Brick House hase :"+stages+" Stages and hase Longitude: "+longitude+" and Latitude:"+latitude);
+    }
+}
---
+++ SMP/Lab11FlyWeight/Zad1House/HouseFactory.cs
@@ -0,0 +1,22 @@
+﻿namespace Zad1House;
+
+public class HouseFactory
+{
+    Dictionary<string, House> houses = new Dictionary<string, House>();
+
+    public HouseFactory()
+    {
+        houses.Add("Panel", new PanelHouse());
+        houses.Add("Brick", new BrickHouse());
+    }
+
+    public House GetHouse(string key)
+    {
+        if (houses.ContainsKey(key))
+        {
+            return houses[key];
+        }
+
+        return null;
+    }
+}
