3d9cfeff9fe1f9b9ffd7aaccedcc7147ba95b644
irumenov2 <ivaylo.rumenov2@dxc.com>
2024-03-08 10:31:28 +0200

    Finished proxy lab

---
+++ SMP/Lab12/zad2/RealClient.cs
@@ -0,0 +1,12 @@
+﻿namespace zad2;
+
+public class RealClient:Iclient
+{
+    public string Data;
+    public string GetData()
+    {
+        Console.WriteLine("Real Client: Initialized");
+        this.Data = "Dot Net Tricks";
+        return this.Data;
+    }
+}
