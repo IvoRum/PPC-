f617eb3d9a10a2f708ac76aa65166ca5bb633a7c
IvoRum <ivailo.rumenov@outlook.com>
2024-03-10 21:19:29 +0200

    Interpret is done

---
+++ SMP/Lab15Interpret/Zad2/Context.cs
@@ -0,0 +1,24 @@
+﻿namespace Zad2;
+
+public class Context
+{
+    string _input;
+    private int _output;
+
+    public Context(string input)
+    {
+        _input = input;
+    }
+
+    public string Input
+    {
+        get => _input;
+        set => _input = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public int Output
+    {
+        get => _output;
+        set => _output = value;
+    }
+}
---
+++ SMP/Lab15Interpret/Zad2/ThousandExpression.cs
@@ -0,0 +1,14 @@
+﻿namespace Zad2;
+
+public class ThousandExpression:Expression
+{
+    public override string One() { return "M"; } 
+
+    public override string Four() { return " "; } 
+
+    public override string Five() { return " "; } 
+
+    public override string Nine() { return " "; } 
+
+    public override int Multiplier() { return 1000; } 
+}
