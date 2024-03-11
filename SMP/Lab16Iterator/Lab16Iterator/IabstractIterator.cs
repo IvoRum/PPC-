5aeb456e2634fe8bfe9dd9217945c489b7f136c8
irumenov2 <ivaylo.rumenov2@dxc.com>
2024-03-11 09:17:03 +0200

    Iterator is done

---
+++ SMP/Lab16Iterator/Lab16Iterator/IabstractIterator.cs
@@ -0,0 +1,12 @@
+﻿namespace Lab16Iterator;
+
+public interface IabstractIterator
+{
+    public Item First(); 
+
+    public Item Next(); 
+
+    public bool IsDone { get; } 
+
+    public Item CurrentItem { get; }
+}
