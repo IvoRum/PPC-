3a72011c46cee28cc48b64352dab8731595b59f4
irumenov2 <ivaylo.rumenov2@dxc.com>
2024-03-11 10:53:04 +0200

    State is done

---
+++ SMP/lab19Observer/lab19Observer/Program.cs
@@ -0,0 +1,15 @@
+﻿using lab19Observer;
+
+public class Program{
+    public static void Main()
+    {
+        IBM ibm = new IBM("IBM", 120.00);
+        ibm.Attach(new Investor("Sorros")); 
+        ibm.Attach(new Investor("Berkshire"));
+        
+        ibm.Price = 120.10; 
+        ibm.Price = 121.00; 
+        ibm.Price = 120.50; 
+        ibm.Price = 120.75; 
+    }
+}
