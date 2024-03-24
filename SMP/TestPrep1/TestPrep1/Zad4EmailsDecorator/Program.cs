bd082b456a8df2dea96943c66093f63aee0c6303
IvoRum <ivailo.rumenov@outlook.com>
2024-03-24 22:15:05 +0200

    Mda

---
+++ SMP/TestPrep1/TestPrep1/Zad4EmailsDecorator/Program.cs
@@ -0,0 +1,21 @@
+﻿using Zad4EmailsDecorator;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create basic email sender
+        IEmailSender basicEmailSender = new EmailSender();
+
+        // Decorate the basic sender with encryption
+        IEmailSender encryptedEmailSender = new EncryptionDecorator(basicEmailSender);
+
+        // Decorate the encrypted sender with signature
+        IEmailSender encryptedAndSignedEmailSender = new SignatureDecorator(encryptedEmailSender);
+
+        // Send email using decorated sender
+        encryptedAndSignedEmailSender.SendEmail("recipient@example.com", "Hello", "This is a secure email");
+
+        Console.ReadLine();
+    }
+}
