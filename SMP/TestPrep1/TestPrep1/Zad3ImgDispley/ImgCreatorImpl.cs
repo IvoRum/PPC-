9f2ef9ef9acd50702c07e2492d0f61850922e3c9
IvoRum <ivailo.rumenov@outlook.com>
2024-03-24 22:05:20 +0200

    Zad 3 is done whit factory

---
+++ SMP/TestPrep1/TestPrep1/Zad3ImgDispley/ImgCreatorImpl.cs
@@ -0,0 +1,9 @@
+﻿namespace Zad3ImgDispley;
+
+public class ImgCreatorImpl:ImgCreator
+{
+    public override Image ImageFactory(int lenght, int hight, string format)
+    {
+        throw new NotImplementedException();
+    }
+}
