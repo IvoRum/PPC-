d5ac2d4a58f0c08a8e5f3ceb16e3b81d23819aeb
IvoRum <ivailo.rumenov@outlook.com>
2024-03-24 22:14:54 +0200

    Zad 4 done whit decorator

---
+++ SMP/TestPrep1/TestPrep1/Zad4EmailsDecorator/EmailDecorator.cs
@@ -0,0 +1,16 @@
+﻿namespace Zad4EmailsDecorator;
+
+public abstract class EmailDecorator : IEmailSender
+{
+    private readonly IEmailSender _emailSender;
+
+    protected EmailDecorator(IEmailSender emailSender)
+    {
+        _emailSender = emailSender;
+    }
+
+    public virtual void SendEmail(string to, string subject, string body)
+    {
+        _emailSender.SendEmail(to, subject, body);
+    }
+}
---
+++ SMP/TestPrep1/TestPrep1/Zad4EmailsDecorator/EmailSender.cs
@@ -0,0 +1,10 @@
+﻿namespace Zad4EmailsDecorator;
+
+public class EmailSender:IEmailSender
+{
+    public void SendEmail(string to, string subject, string body)
+    {
+        Console.WriteLine($"Sending email to: {to}\nSubject: {subject}\nBody: {body}");
+        // Logic to send email...
+    }
+}
