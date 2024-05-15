583022ce495c3c96c2a6f024d7f2b3d81adcafbf
IvoRum <ivailo.rumenov@outlook.com>
2024-05-15 21:26:43 +0300

    Created zad1

---
+++ SMP/TestPrep2/KontrolnoVarian1/Zad1/Program.cs
@@ -0,0 +1,14 @@
+﻿using Zad1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Calculator calculator = new Calculator();
+        calculator.sum(4,5);
+        calculator.sum(5,3);
+        calculator.sum(3,12);
+        calculator.sum(100,5);
+        calculator.getMementos();
+    }
+}
